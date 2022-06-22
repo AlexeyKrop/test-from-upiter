35cc7b492b1d74be626775078a65ebe51f4fa627
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-06-22 12:01:36 +0300

    add func card active

--- src/components/Main/Cards/Card/Card.tsx
+++ src/components/Main/Cards/Card/Card.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './Card.module.css'
+
 export const Card = (props: any) => {
+  const[toggle, setToggle] = useState<boolean>(false)
+  console.log(toggle)
   const onClickHandler = (value: string) => {
     props.onClickFilterGoods(value)
   }
+  const onClickCardActiveHandler = () => {
+    setToggle(!toggle)
+  }
   return (
-    <div className={s.wrapper}>
+    <div className={!toggle ? `${s.wrapper}` : `${s.wrapper} ${s.cardActive}`} onClick={onClickCardActiveHandler}>
       <div className={s.filterGroup}>
         <button className={s.btn} onClick={()=>onClickHandler(props.filter)} name={props.filter}>{props.filter}</button>
         <p className={s.name}>{props.name}</p>
