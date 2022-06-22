c0db896154276823213799ee8621e2b06b6428cd
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-06-22 11:50:49 +0300

    add onClickHandler in Card.tsx

--- src/components/Main/Cards/Card/Card.tsx
+++ src/components/Main/Cards/Card/Card.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import s from './Card.module.css'
 export const Card = (props: any) => {
+  const onClickHandler = (value: string) => {
+    console.log(value)
+  }
   return (
     <div className={s.wrapper}>
       <div className={s.filterGroup}>
-        <button className={s.btn} onClick={()=>{}} name={props.filter}>{props.filter}</button>
+        <button className={s.btn} onClick={()=>onClickHandler(props.filter)} name={props.filter}>{props.filter}</button>
         <p className={s.name}>{props.name}</p>
       </div>
 
