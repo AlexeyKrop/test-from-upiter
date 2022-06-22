bde3429e1301cb56e9a1d7130bd7e0408fdbb741
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-06-22 14:37:51 +0300

    add image

--- src/components/Main/Cards/Card/Card.tsx
+++ src/components/Main/Cards/Card/Card.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import s from './Card.module.css'
 export type CardPropsType = {
-  good: {id: number, name: string, filter: string},
+  good: {id: number, name: string, filter: string, src: string},
   deleteGood: (id: number) => void,
   filter: string,
   id: number,
@@ -10,7 +10,6 @@ export type CardPropsType = {
   tabIndex: number,
 }
 export const Card = (props: CardPropsType) => {
-  console.log(props)
   const[toggle, setToggle] = useState<boolean>(false)
   const onClickHandler = (value: string) => {
     props.onClickFilterGoods(value)
@@ -24,6 +23,7 @@ export const Card = (props: CardPropsType) => {
   }
   return (
     <div tabIndex={props.tabIndex} onKeyDown={(e) => {if(e.key === 'Delete') {onKeyPressHandler(props.id)}}} className={!toggle ? `${s.wrapper}` : `${s.wrapper} ${s.cardActive}`} onClick={onClickCardActiveHandler}>
+        <img className={s.img} src={props.good.src} alt="cardImage"/>
       <div className={s.filterGroup}>
         <button className={s.btn} onClick={()=>onClickHandler(props.filter)} name={props.filter}>{props.filter}</button>
         <p className={s.name}>{props.name}</p>
--- src/components/Main/Goods.tsx
+++ src/components/Main/Goods.tsx
@@ -3,27 +3,27 @@ import {Cards} from "./Cards/Cards";
 import s from './Goods.module.css'
 
 export type GoodType = {
-  id: number, name: string, filter: string
+  id: number, name: string, filter: string, src: string
 }
 const defaultGoods: Array<GoodType> = [
-  {id: 1, name: 'SOFA', filter: 'Design'},
-  {id: 2, name: 'KeyBoard', filter: 'Branding'},
-  {id: 3, name: 'Work Media', filter: 'Illustration'},
-  {id: 4, name: 'DDDone', filter: 'Motion'},
-  {id: 5, name: 'Abstract', filter: 'Design'},
-  {id: 6, name: 'HandP', filter: 'Branding'},
-  {id: 7, name: 'Architect', filter: 'Illustration'},
-  {id: 8, name: 'CalC', filter: 'Motion'},
-  {id: 9, name: 'Sport', filter: 'Design'},
-  {id: 10, name: 'html & css', filter: 'Branding'},
-  {id: 11, name: 'html & css', filter: 'Illustration'},
-  {id: 12, name: 'html & css', filter: 'Motion'},
-  {id: 13, name: 'html & css', filter: 'Design'},
-  {id: 14, name: 'html & css', filter: 'Branding'},
-  {id: 15, name: 'html & css', filter: 'Illustration'},
-  {id: 16, name: 'html & css', filter: 'Motion'},
-  {id: 17, name: 'html & css', filter: 'Design'},
-  {id: 18, name: 'html & css', filter: 'Motion'},
+  {id: 1, name: 'SOFA', src:"https://supersnimki.ru/images/pub/2017/11/20/343708e7-ce20-11e7-9923-90b50f07f13d_original.jpg?556747", filter: 'Design'},
+  {id: 2, name: 'KeyBoard',src:"https://avatars.mds.yandex.net/get-zen_doc/1587994/pub_5d7df76035c8d800ad8f0e69_5d82362a8f011100b7d48b09/scale_1200", filter: 'Branding'},
+  {id: 3, name: 'Work Media',src:"https://img-fotki.yandex.ru/get/105020/198922885.9e/0_1c30d9_4f645798_orig.jpg", filter: 'Illustration'},
+  {id: 4, name: 'DDDone',src:"https://travel-dom.ru/wp-content/uploads/2019/08/sibir1.jpg", filter: 'Motion'},
+  {id: 5, name: 'Abstract', src:"https://webkamerton.ru/sites/default/files/wp-content/thumbnails/skrinshot-13-09-2021-103546.jpg",filter: 'Design'},
+  {id: 6, name: 'HandP', src:"https://photocentra.ru/blog/1069/1.jpg", filter: 'Branding'},
+  {id: 7, name: 'Architect', src:"https://gamerwall.pro/uploads/posts/2022-06/1655812215_53-gamerwall-pro-p-sibir-vesnoi-oboi-63.jpg",filter: 'Illustration'},
+  {id: 8, name: 'CalC', src:"https://proprikol.ru/wp-content/uploads/2020/07/kartinki-zimnij-vecher-55.jpg",filter: 'Motion'},
+  {id: 9, name: 'Sport', src:"https://avatars.mds.yandex.net/get-zen_doc/1328466/pub_60fbb08b51197c3f21ea9a6a_60fbb08d237ef804672941ef/scale_1200",filter: 'Design'},
+  {id: 10, name: 'River', src:"https://posibiri.ru/wp-content/uploads/2020/04/030420.jpg",filter: 'Branding'},
+  {id: 11, name: 'Lake', src:"https://gamerwall.pro/uploads/posts/2022-06/1655685008_26-gamerwall-pro-p-sibir-ozero-baikal-priroda-krasivo-foto-26.jpg",filter: 'Illustration'},
+  {id: 12, name: 'Forest', src:"https://gamerwall.pro/uploads/posts/2022-06/1655684985_50-gamerwall-pro-p-sibir-ozero-baikal-priroda-krasivo-foto-55.jpg",filter: 'Motion'},
+  {id: 13, name: 'Sea', src:"https://gamerwall.pro/uploads/posts/2022-06/1655685006_41-gamerwall-pro-p-sibir-ozero-baikal-priroda-krasivo-foto-42.jpg",filter: 'Design'},
+  {id: 14, name: 'mountain', src:"https://travelpoisk.ru/data/wikitravel/22/sibir.jpg",filter: 'Branding'},
+  {id: 15, name: 'village', src:"https://photocentra.ru/images/main71/715466_main.jpg",filter: 'Illustration'},
+  {id: 16, name: 'snow', src:"https://proprikol.ru/wp-content/uploads/2020/07/kartinki-zimnij-vecher-44.jpg",filter: 'Motion'},
+  {id: 17, name: 'Night', src:"https://avatars.mds.yandex.net/get-zen_doc/30884/pub_5c380ac35c17e000a9861d68_5c382f17561f7a00aa62eef2/scale_1200",filter: 'Design'},
+  {id: 18, name: 'Evening', src:"https://proprikol.ru/wp-content/uploads/2020/07/kartinki-zimnij-vecher-58.jpg",filter: 'Motion'},
 ]
 
 export const filterGoods = (goods: Array<GoodType>, filter: string): Array<GoodType> => {
